c2a8d20f0315a83dfd932aa9323de8c1702d891d
Xico26 <47830278+Xico26@users.noreply.github.com>
2023-09-13 14:57:08 +0100

    Update db.js

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const pool = new Pool({
     user: process.env.DB_USER,
     password: process.env.DB_PASS,
     host: process.env.DB_HOST,
-    database: "tasksapp_jvip",
+    database: process.env.DB_NAME,
     port: 5432
 })
 
